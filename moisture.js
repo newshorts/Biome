f0b57be39fc40bdc4117e25b264941560a9bcb26
Mike Newell <mike_newell@gspsf.com>
2014-09-12 18:06:03 -0700

    added changes to moisture

--- moisture.js
+++ moisture.js
@@ -1,4 +1,6 @@
-var express = require('express')
+var express = require('express'),
+    gpio = require("pi-gpio");
+    
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
@@ -6,8 +8,15 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
-})
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
+
+gpio.open(16, "input", function(err) {
+    gpio.read(16, function(err, val) {
+        if(err) throw err;
+        console.log(val);
+    });
+});
